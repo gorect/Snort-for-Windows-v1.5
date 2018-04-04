efd420145bf30d2559405459b2a4e81831f64e34
gorect <gorect@vcu.edu>
2018-04-04 16:11:51 -0400

    Still some issues with speeding up the program using a dictionary approch

---
+++ Snort-for-Windows-v1.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snort_for_Windows_v1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
